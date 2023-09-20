70e01702da4f6128c4c6299253eaa91954d82167
Ruan Bertin <ruanbertin@hotmail.com>
2023-09-20 16:02:22 -0300

    Adicionando exercicio typescript

---
+++ exercicio_ts/exercicio_ts.ts
@@ -0,0 +1,6 @@
+const multiplicacao = (numero1: number, numero2: number): number =>  numero1 * numero2;;
+
+const dizOi = (nome:string): string => `${nome} diz oi`;
+
+console.log(dizOi("Ruan"));
+console.log(multiplicacao(5,8));
